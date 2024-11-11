9bae6655ef3e85290a7b89ca08c0de2fe9e0b991
Shamimapoly <shamimapoly365@gmail.com>
2024-11-11 19:42:47 +0600

    a

--- app/componants/Bannar.jsx
+++ app/componants/Bannar.jsx
@@ -4,10 +4,14 @@ import Image from 'next/image'
 const Bannar = () => {
   return (
     <div className=''>
-      a
+     
       <Image className='w-full h-[873px] bg-cover bg-bottom' src={bannar} alt={bannar}/>
     </div>
   )
 }
 
 export default Bannar
+
+
+
+// -left-[139px]
